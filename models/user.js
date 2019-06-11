2412b646bb3ac68d01f60dda4dd7a860befc93ce
Quan <quannm@bit-vietnam.com>
2019-06-11 13:54:51 +0700

    commit code

---
+++ models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+var schema = new Schema({
+  username: {type:String, require:true},
+  fullname: {type:String, require:true},
+  email: {type:String, require:true},
+  phone: {type:String, require:true},
+  password: {type:String, require:true},
+  creation_dt: {type:Date, require:true}
+});
+
+schema.statics.hashPassword = function hashPassword(password){
+  return bcrypt.hashSync(password,10);
+}
+
+schema.methods.isValid = function(hashedpassword){
+  return bcrypt.compareSync(hashedpassword, this.password);
+}
+
+module.exports = mongoose.model('User', schema);
